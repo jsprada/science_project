1080f22317726568e0513b34427e607a8e2d0213
jsprada <jsprada@gmail.com>
2018-11-19 20:02:03 -0800

    add simple instructions to control poll frequency

--- temps.py
+++ temps.py
@@ -3,12 +3,22 @@ import re
 from datetime import datetime
 import time
 
-
+# this script is called once per minute, by cron.
+# to increase the number of times the sensors are polled per run
+# edit the interval to the number of additional rounds
+# and delay to the number of seconds between rounds.  
+# It takes about 5-6 seconds for the pi to poll 4 sensors per round.  
+# So adding a 15 second delay, and interval of 2 will effectively create 
+# one poll every 20 seconds(ish)
+# set interval to 0 for once per minute
+# set interval to 1 and delay to 25 for twice per minute
+# set interval to 2 and delay to 15 for thrice per minute
+# It is not recommended to poll more often than that.
 
 interval = 2 # number of times to run after first run
 delay = 15 # delay between running (takes about 5-6 seconds each poll)
 header = ['datetime', 'sensor_1', 'sensor_2', 'sensor_3', 'sensor_4']
-log_file = '/home/pi/sp/temps_multi_log'
+log_file = '/home/pi/sp/temps_log'
 w1_path = '/sys/bus/w1/devices/'
 
 counter = 0
